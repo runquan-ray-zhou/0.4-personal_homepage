5ad76d9a3984a2da39865118bcee50631b5cfbb5
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-05-21 16:35:11 -0400

    change dropdown menu style

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -32,20 +32,20 @@ export default function Nav() {
             <li>
                 <span onClick={handleGroupDisplayChange}>GROUP PROJECTS</span>
                 <div className="dropDown" style={{display:groupDisplay}}>
-                <span><a href="https://mynotes-app-project.netlify.app/">NOTES APP</a></span>
-                <span><a href="https://therealyoutube.netlify.app/">YOUTUBE CLONE</a></span>
+                <span><a href="https://mynotes-app-project.netlify.app/">0.7 - NOTES APP</a></span>
+                <span><a href="https://therealyoutube.netlify.app/">0.9 - YOUTUBE CLONE</a></span>
                 </div>
             </li>
             <li>
                 <span onClick={handlePortfolioDisplayChange}>PORTFOLIO</span>
                 <div className="dropDown" style={{display:portfolioDisplay}}>
-                <span><a href="https://squadtrack.netlify.app/">SQUAD TRACK</a></span>
-                <span><a href="https://runquan-ray-zhou.github.io/0.2-connectfour/">CONNECT 4</a></span>
-                <span><a href="https://anaira.netlify.app/">SQUAD HOME PAGE</a></span>
-                <span><a href="https://therealpokedex.netlify.app/">POKEDEX</a></span>
-                <span><a href="https://cowsaywhat.netlify.app/">COW SAY</a></span>
-                <span><a href="https://quiz-me-trivia-app.netlify.app/">QUIZ ME</a></span>
-                <span><a href="https://pocket-dictionary-app.netlify.app/">DICTIONARY</a></span>
+                <span><a href="https://squadtrack.netlify.app/">0.1 - SQUAD TRACK</a></span>
+                <span><a href="https://runquan-ray-zhou.github.io/0.2-connectfour/">0.2 - CONNECT 4</a></span>
+                <span><a href="https://anaira.netlify.app/">0.3 - SQUAD HOME PAGE</a></span>
+                <span><a href="https://therealpokedex.netlify.app/">0.4 - POKEDEX</a></span>
+                <span><a href="https://cowsaywhat.netlify.app/">0.6 - COW SAY</a></span>
+                <span><a href="https://quiz-me-trivia-app.netlify.app/">0.8 - QUIZ ME</a></span>
+                <span><a href="https://pocket-dictionary-app.netlify.app/">1.0 - DICTIONARY</a></span>
                 </div>
             </li>
         </ul>
