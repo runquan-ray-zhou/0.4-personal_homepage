fda0e022132fc75c3024d80dd0dc9333e18e0e3e
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-05-21 10:46:10 -0400

    update site

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,13 +2,22 @@ import { useState } from "react"
 
 export default function Nav() {
 
-    const [display, setDisplay] = useState("none")
+    const [groupDisplay, setGroupDisplay] = useState("none")
+    const [portfolioDisplay, setPortfolioDisplay] = useState("none")
 
-    function handleDisplayChange() {
-        if (display === "none") {
-            setDisplay("flex")
+    function handleGroupDisplayChange() {
+        if (groupDisplay === "none") {
+            setGroupDisplay("flex")
         } else {
-            setDisplay("none")
+            setGroupDisplay("none")
+        }
+    }
+
+    function handlePortfolioDisplayChange() {
+        if (portfolioDisplay === "none") {
+            setPortfolioDisplay("flex")
+        } else {
+            setPortfolioDisplay("none")
         }
     }
 
@@ -19,15 +28,24 @@ export default function Nav() {
         <ul>
             <li><a href="">HOME</a></li>
             <li><a href="">ABOUT</a></li>
-            <li><a href="">SERVICES</a></li>
             <li><a href="">HIRE ME</a></li>
             <li>
-                <span onClick={handleDisplayChange}>PORTFOLIO</span>
-                <div className="dropDown" style={{display:display}}>
+                <span onClick={handleGroupDisplayChange}>GROUP PROJECTS</span>
+                <div className="dropDown" style={{display:groupDisplay}}>
+                <span><a href="https://mynotes-app-project.netlify.app/">NOTES APP</a></span>
+                <span><a href="https://therealyoutube.netlify.app/">YOUTUBE CLONE</a></span>
+                </div>
+            </li>
+            <li>
+                <span onClick={handlePortfolioDisplayChange}>PORTFOLIO</span>
+                <div className="dropDown" style={{display:portfolioDisplay}}>
                 <span><a href="https://squadtrack.netlify.app/">SQUAD TRACK</a></span>
                 <span><a href="https://runquan-ray-zhou.github.io/0.2-connectfour/">CONNECT 4</a></span>
-                <span><a href="https://anaira.netlify.app/">ANAIRA HOME PAGE</a></span>
-                <span><a href="https://therealpokedex.netlify.app/">ALEXANDER'S POKEDEX</a></span>
+                <span><a href="https://anaira.netlify.app/">SQUAD HOME PAGE</a></span>
+                <span><a href="https://therealpokedex.netlify.app/">POKEDEX</a></span>
+                <span><a href="https://cowsaywhat.netlify.app/">COW SAY</a></span>
+                <span><a href="https://quiz-me-trivia-app.netlify.app/">QUIZ ME</a></span>
+                <span><a href="https://pocket-dictionary-app.netlify.app/">DICTIONARY</a></span>
                 </div>
             </li>
         </ul>
