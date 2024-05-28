b0a72d853c1ea03e88ad6b66ff6a612d8c136116
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-05-28 02:51:36 -0400

    add link to resume

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -5,7 +5,7 @@ export default function Info() {
     <div className="info">
         <h1>I'm Runquan <br/> <span>(Ray)</span> Zhou</h1>
         <p>This is my official portfolio website to showcase all my works related to web<br/> development and UI design.</p>
-        <a href="">Show CV</a>
+        <a href="https://docs.google.com/document/d/1gPRbDrXuQstrpOAOm4gtzAvWm9T_-6v2haQFfsH0ZDA/edit">Show CV</a>
     </div>
 
     )
