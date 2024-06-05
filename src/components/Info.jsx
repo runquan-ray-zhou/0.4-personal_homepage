2694cb2f8b6bff531482fc49d52954d9c2958c41
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-06-05 10:25:42 -0400

    add quote

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,7 +4,7 @@ export default function Info() {
 
     <div className="info">
         <h1>I'm Runquan <br/> <span>(Ray)</span> Zhou</h1>
-        <p>This is my official portfolio website to showcase all my works related to web<br/> development and UI design.</p>
+        <p>Driven by Challenges, Fueled by Creativity.</p>
         <a href="https://docs.google.com/document/d/1gPRbDrXuQstrpOAOm4gtzAvWm9T_-6v2haQFfsH0ZDA/edit">Show CV</a>
     </div>
 
