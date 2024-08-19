d43da7d89842346d81619d51460814cea496fb3f
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-08-19 02:25:47 -0400

    add download link for resume

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -5,8 +5,9 @@ export default function Info() {
     <div className="info">
         <h1>I'm Runquan <br/> <span>(Ray)</span> Zhou</h1>
         <p>Driven by Challenges, Fueled by Creativity.</p>
-        <a href="https://docs.google.com/document/d/1PU9O1aSlj2VsTaWBED22dhGutsqY4YopTi5Uoedt4-s/edit#heading=h.81gx5oix5oym">Show CV</a>
-        {/* <a href="https://drive.google.com/file/d/1-yBjv43IzOu1chWG8mTnLEMd5OWdfdI-/view?usp=drive_link" download>Download CV</a> */}
+        {/* <a href="https://docs.google.com/document/d/1PU9O1aSlj2VsTaWBED22dhGutsqY4YopTi5Uoedt4-s/edit#heading=h.81gx5oix5oym">Show CV</a> */}
+        <a href="https://drive.google.com/file/d/1PU9O1aSlj2VsTaWBED22dhGutsqY4YopTi5Uoedt4-s/view" target="_blank">View CV</a>
+        <a href="https://docs.google.com/document/d/1PU9O1aSlj2VsTaWBED22dhGutsqY4YopTi5Uoedt4-s/export?format=pdf" download>Download CV</a>
     </div>
 
     )
