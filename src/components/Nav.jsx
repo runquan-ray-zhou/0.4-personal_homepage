88ff770a9adfaab2d70db5db78af64a5f720c8cc
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-08-23 01:21:08 -0400

    replace logo, add projects, and correct drop down bug

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import logo from "../assets/Screenshot 2024-08-23 at 12.46.54 AM.png"
 
 export default function Nav() {
 
@@ -8,6 +9,7 @@ export default function Nav() {
     function handleGroupDisplayChange() {
         if (groupDisplay === "none") {
             setGroupDisplay("flex")
+            setPortfolioDisplay("none")
         } else {
             setGroupDisplay("none")
         }
@@ -16,37 +18,55 @@ export default function Nav() {
     function handlePortfolioDisplayChange() {
         if (portfolioDisplay === "none") {
             setPortfolioDisplay("flex")
+            setGroupDisplay("none")
         } else {
             setPortfolioDisplay("none")
         }
     }
 
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (!event.target.closest('.group-container') && !event.target.closest('a')) {
+                setGroupDisplay("none");
+                setPortfolioDisplay("none");
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
     return (
 
     <nav>
-        <a href="https://www.pursuit.org"><img src="https://pursuit.codetrack.dev/pursuit_logo.png" className="logo" /></a>
+        <a href="https://runquanrayzhou.netlify.app/"><img src={logo} className="logo" /></a>
         <ul>
             <li><a href="">HOME</a></li>
             <li><a href="">ABOUT ME</a></li>
             <li>
                 <span onClick={handleGroupDisplayChange}>GROUP PROJECTS</span>
-                <div className="dropDown" style={{display:groupDisplay}}>
-                <span><a href="https://mynotes-app-project.netlify.app/">0.7 - NOTES APP</a></span>
-                <span><a href="https://therealyoutube.netlify.app/">0.9 - YOUTUBE CLONE</a></span>
-                </div>
+                    <div className="dropDown" style={{display:groupDisplay}}>
+                        <span><a href="https://mynotes-app-project.netlify.app/">0.7 - NOTES APP</a></span>
+                        <span><a href="https://therealyoutube.netlify.app/">0.9 - YOUTUBE CLONE</a></span>
+                        <span><a href="https://streetbites.netlify.app/">1.6 - STREETBITES APP</a></span>
+                    </div>
+                    
             </li>
             <li>
                 <span onClick={handlePortfolioDisplayChange}>PORTFOLIO</span>
-                <div className="dropDown" style={{display:portfolioDisplay}}>
-                <span><a href="https://squadtrack.netlify.app/">0.1 - SQUAD TRACK</a></span>
-                <span><a href="https://runquan-ray-zhou.github.io/0.2-connectfour/">0.2 - CONNECT 4</a></span>
-                <span><a href="https://anaira.netlify.app/">0.3 - SQUAD HOME PAGE</a></span>
-                <span><a href="https://therealpokedex.netlify.app/">0.4 - POKEDEX</a></span>
-                <span><a href="https://cowsaywhat.netlify.app/">0.6 - COW SAY</a></span>
-                <span><a href="https://quiz-me-trivia-app.netlify.app/">0.8 - QUIZ ME</a></span>
-                <span><a href="https://pocket-dictionary-app.netlify.app/">1.0 - DICTIONARY</a></span>
-                <span><a href="https://linknyc-finder.netlify.app/">2.9 - LinkNYC FINDER</a></span>
-                </div>
+                        <div className="dropDown" style={{display:portfolioDisplay}}>
+                            <span><a href="https://squadtrack.netlify.app/">0.1 - SQUAD TRACK</a></span>
+                            <span><a href="https://runquan-ray-zhou.github.io/0.2-connectfour/">0.2 - CONNECT 4</a></span>
+                            <span><a href="https://anaira.netlify.app/">0.3 - SQUAD HOME PAGE</a></span>
+                            <span><a href="https://therealpokedex.netlify.app/">0.4 - POKEDEX</a></span>
+                            <span><a href="https://cowsaywhat.netlify.app/">0.6 - COW SAY</a></span>
+                            <span><a href="https://quiz-me-trivia-app.netlify.app/">0.8 - QUIZ ME</a></span>
+                            <span><a href="https://pocket-dictionary-app.netlify.app/">1.0 - DICTIONARY</a></span>
+                            <span><a href="https://bankbankster.netlify.app/">1.1 - BANK BANKSTER</a></span>
+                            <span><a href="https://linknyc-finder.netlify.app/">2.9 - LinkNYC FINDER</a></span>
+                        </div>
             </li>
         </ul>
     </nav>
