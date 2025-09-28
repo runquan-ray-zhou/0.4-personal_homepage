713f73fe69f5bdedf4a9d90aab9cc1f8912954f8
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2025-09-27 22:29:52 -0400

    update nav bar, remove unused assets

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,19 @@
 import { useNavigate } from "react-router-dom";
-import logo from "../assets/Screenshot 2024-08-23 at 12.46.54 AM.png";
 
 export default function Nav() {
   let navigate = useNavigate();
 
   return (
     <nav className="flex items-center justify-between px-4 py-4 bg-gray-800 text-white">
-      <div onClick={() => navigate(`/`)}>
-        <img
-          src={logo}
-          className="w-24 h-12 object-cover rounded-lg cursor-pointer"
-          alt="Logo"
-        />
-      </div>
       <ul className="relative z-50 flex space-x-6">
+        <li className="relative ml-6 md:ml-0">
+          <span
+            onClick={() => navigate(`/`)}
+            className="cursor-pointer hover:text-gray-300"
+          >
+            Home
+          </span>
+        </li>
         <li className="relative ml-6 md:ml-0">
           <span
             onClick={() => navigate(`/projects`)}
